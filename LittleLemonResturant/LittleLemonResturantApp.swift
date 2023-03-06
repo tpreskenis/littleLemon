12e16a93be6e19e1ca1e417e9b2a92028e21cc3c
Preskenis, Tim <tpreskenis@abiomed.com>
2023-03-06 17:18:12 -0500

    Initial Commit

---
+++ LittleLemonResturant/LittleLemonResturantApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonResturantApp.swift
+//  LittleLemonResturant
+//
+//  Created by Tim Preskenis on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonResturantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
